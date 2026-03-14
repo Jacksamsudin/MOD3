e52ba4fb056690753e08bc23cdebb3b2dacc7763
Tegar <tegars444@gmail.com>
2026-03-14 08:47:11 +0700

    Menambahkan implementasi konversi suhu

--- Form1.cs
+++ Form1.cs
@@ -39,7 +39,38 @@ namespace Modul3_103022400074
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null) {
+                MessageBox.Show("Pilih satuan terlebih dahulu");
+                    return;
+            }
+            if (textBox1.Text == "") {
+                MessageBox.Show("Masukan angka yang valid!");
+                return;
+            }
+            double nilai = Convert.ToDouble(textBox1.Text);
 
+            String a = comboBox1.SelectedItem.ToString();
+            String b = comboBox2.SelectedItem.ToString();
+
+            double celcius = nilai;
+
+            if (a == "Fahrenheit")
+                celcius = (nilai - 32) * 5 / 9;
+            else if (a == "Kelvin")
+                celcius = nilai - 273.15;
+            else if (a == "Reamur")
+                celcius = nilai * 5 / 4;
+
+            double hasil = celcius;
+
+            if (b == "Fahrenheit")
+                hasil = (celcius * 9 / 5) + 32;
+            else if (b == "Kelvin")
+                hasil = celcius + 273.15;
+            else if (b == "Reamur")
+                hasil = celcius * 4 / 5;
+
+            textBox2.Text = hasil.ToString();
         }
     }
 }
